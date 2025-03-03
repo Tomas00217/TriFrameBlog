dfae929cbe84182e9a631ada188b33e21fa8423d
Tomáš Štucka <Tomas00217@gmail.com>
2025-03-03 13:07:17 +0100

    feat(django): accounts view tests and changes

--- django_blog/accounts/forms.py
+++ django_blog/accounts/forms.py
@@ -2,6 +2,9 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 from .models import EmailUser
 
+class EmailUserLoginForm(forms.Form):
+    email = forms.EmailField(max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput)
 
 class EmailUserCreationForm(UserCreationForm):
     class Meta:
