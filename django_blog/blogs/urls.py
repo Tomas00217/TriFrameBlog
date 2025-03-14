019130a3fdc14a9e559290a639d39f7aa5a03362
Tomáš Štucka <Tomas00217@gmail.com>
2025-03-14 19:12:09 +0100

    refactor(django): change blog url to blogs

--- django_blog/blogs/urls.py
+++ django_blog/blogs/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("blog/", views.blogs, name="blogs"),
-    path("blog/<int:blog_id>", views.detail, name="detail"),
-    path("blog/create", views.create, name="create"),
-    path("blog/<int:blog_id>/edit", views.edit, name="edit"),
-    path("blog/<int:blog_id>/delete", views.delete, name="delete"),
-    path("blog/my", views.my_blogs, name="my_blogs"),
+    path("blogs/", views.blogs, name="blogs"),
+    path("blogs/<int:blog_id>", views.detail, name="detail"),
+    path("blogs/create", views.create, name="create"),
+    path("blogs/<int:blog_id>/edit", views.edit, name="edit"),
+    path("blogs/<int:blog_id>/delete", views.delete, name="delete"),
+    path("blogs/my", views.my_blogs, name="my_blogs"),
 ]
